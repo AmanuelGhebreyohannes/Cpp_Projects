503be29f273ca3363d2dde7aa5644895e4eefbe8
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-02-21 15:23:21 -0500

    Basic Operations

---
+++ BasicOperations/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int num1=2;
+    int num2=7;
+    int result = num1+num2;
+    std::cout << "result : "<<result<<std::endl;
+    result = num1-num2;
+    std::cout << "result : "<<result<<std::endl;
+    result = num1*num2;
+    std::cout << "result : "<<result<<std::endl;
+    result = num1/num2;
+    std::cout << "result : "<<result<<std::endl;
+    result = num1%num2;
+    std::cout << "result : "<<result<<std::endl;
+
+    return 0;
+}
