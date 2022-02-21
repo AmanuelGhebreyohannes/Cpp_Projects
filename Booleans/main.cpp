28e30b8cd259bc2daf57269fc95276043215bde4
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-02-21 15:24:05 -0500

    BasicOperations

---
+++ Booleans/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    bool red_light = true;
+    bool green_light = false;
+
+    if ( red_light == true)
+    {
+        std::cout << "Stop! " << std::endl;
+        
+    }
+    else{
+        std::cout << "Go Through"<<std::endl;
+    }
+
+    std::cout << "Sizeof(bool) = "<<sizeof(bool)<<std::endl;
+    std::cout << "red_light = "<<red_light << std::endl;
+    std::cout << std::boolalpha;
+    std::cout << "red_light = "<<red_light << std::endl;
+    return 0;
+
+}
