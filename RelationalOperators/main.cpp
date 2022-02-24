f269857cc2c3348fcd42677964311c1ad92ca02e
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-02-23 20:51:19 -0500

    Relational Operators

---
+++ RelationalOperators/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int num1 = 50;
+    int num2 = 60;
+    
+    std::cout << std::boolalpha;
+    std::cout<<" num1 < num2 : "<<(num1<num2)<<std::endl;
+    return 0;
+}
