df5150a4ba613de90f179711af82834539b6ed2d
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-02-24 15:19:42 -0500

    IfStatements

---
+++ IfStatements/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    bool resutl = (55 < 60);
+    if(resutl == true)
+    std::cout << 55 <<std::endl;
+    else if (resutl == false)
+    std::cout << 60 << std::endl;
+    else std::cout << 60 << std::endl;    
+
+    return 0;
+}
