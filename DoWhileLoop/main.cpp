583a73f9098c2584992f4b61e3d62290f0017373
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-02-27 18:23:38 -0500

    do while loop

---
+++ DoWhileLoop/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    const int count = 10;
+    size_t i = 0;
+
+    do{
+        std::cout << i << " : I love c++"<<std::endl;
+        i++;
+    }while(i<count);
+
+    std::cout<<" Loop is done!"<<std::endl;
+    return 0;
+}
