77691ac47b0409592741d1b89a1d666164e3a485
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-03-03 19:47:24 -0500

    Null Pointer safety

--- NullPointerSafety/main.cpp
+++ NullPointerSafety/main.cpp
@@ -2,5 +2,25 @@
 
 int main()
 {
+    // int *p_number;
+    // if(!(p_number == nullptr))
+    //     std::cout << p_number <<std::endl;
+    // else std::cout<<"invalid address"<<std::endl;
+
+    int *p_number;
+    //p_number = new int(7);
+    if(!(p_number == nullptr))
+        std::cout << p_number <<std::endl;
+    else std::cout<<"invalid address"<<std::endl;
+
+    delete p_number;
+    nullptr;
+
+    int *p_number1;
+    //delete p_number1;
+    //p_number1 = nullptr;
+    if(p_number1 != nullptr)
+         delete p_number1;
+
     return 0;
 }
