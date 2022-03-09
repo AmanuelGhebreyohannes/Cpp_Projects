5ad077097b2c82347241e10eb0639b12b181bdb0
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-03-08 21:16:39 -0500

    Functions

---
+++ Functions/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+
+void enter_bar(unsigned int age)
+{
+    if(age > 10)
+        std::cout <<age << std::endl;
+    else
+        std::cout<<"Sorry"<<std::endl;
+
+    return ;
+}
+
+int main()
+{
+    // enter_bar(12);
+    // enter_bar(2);
+
+    for(size_t i = 1; i<20;i++)
+    {
+        enter_bar(i);
+
+    }
+    std::cout << sizeof(size_t)<<std::endl;
+    return 0;
+}
