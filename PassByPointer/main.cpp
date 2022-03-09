376166755f130db4e00640d5a3b55bc932d774e2
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-03-08 21:40:54 -0500

    pass by pointers

---
+++ PassByPointer/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void say_age(int* age)
+{
+    ++*age;
+    std::cout << &age << " " <<*age << std::endl;
+}
+
+int main()
+{
+    int age = 33;
+    std::cout << &age << " " <<age << std::endl;
+    say_age(&age);
+
+    std::cout << &age << " " <<age << std::endl;
+    return 0;
+}
