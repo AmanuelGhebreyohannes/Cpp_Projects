3a575eedb81db74581431823057d8203f4663cc1
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-03-08 21:31:30 -0500

    multiple files compilation model

---
+++ MultipleFilesCompilationModelRevisited/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "compare.h"
+
+int main()
+{
+    int max_num = max(34,56);
+    std::cout << max_num<<std::endl;
+
+    int min_um = min(34,56);
+    std::cout << min_um <<std::endl;
+    return 0;
+}
+
