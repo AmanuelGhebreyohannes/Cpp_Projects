908119ec6e6310864460493cec15b9e48b6c8fbf
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-04-12 21:46:26 -0400

    Static Binding with Inheritance

---
+++ StaticBindingWithInheritance/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "shape.h"
+#include "oval.h"
+#include "circle.h"
+
+int main()
+{
+    Shape shape1("Shape1");
+    //shape1.draw();
+
+    Oval oval1(2.0,3.4, "oval1");
+    //oval1.draw();
+
+    Circle circle1(3.3, "circle1");
+    //circle1.draw();
+
+    //Base pointers
+
+    Shape * shape_ptr = &shape1;
+    shape_ptr->draw();
+
+    shape_ptr = & oval1;
+    shape_ptr->draw();
+
+    shape_ptr = & circle1;
+    shape_ptr->draw();
+
+    //Base references
+    
+
+    return 0;
+}
