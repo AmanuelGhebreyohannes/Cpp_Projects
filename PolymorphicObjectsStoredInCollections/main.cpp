7f10e7f028d80db57c948fdaa7503c23f51bbe4f
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-04-14 20:44:33 -0400

    avoiding slicing by using raw pointers

--- PolymorphicObjectsStoredInCollections/main.cpp
+++ PolymorphicObjectsStoredInCollections/main.cpp
@@ -14,28 +14,28 @@ int main()
     Circle circle3(3,"circle3");
     Oval oval3(4,5, "oval3");
 
-    std::cout << " sizeof(circle1) : "<< sizeof(circle1) << std::endl;
+    // std::cout << " sizeof(circle1) : "<< sizeof(circle1) << std::endl;
 
-    Shape shape1[] = {circle1,oval1,circle2,oval2,circle3, oval3};
+    // Shape shape1[] = {circle1,oval1,circle2,oval2,circle3, oval3};
 
-    for (Shape & s : shape1)
-    {
-        std::cout << " sizeof(s) : "<< sizeof(s) << std::endl;
+    // for (Shape & s : shape1)
+    // {
+    //     std::cout << " sizeof(s) : "<< sizeof(s) << std::endl;
 
-        s.draw();
-    }
+    //     s.draw();
+    // }
 
     //can not have array of references
 
     // raw pointers
     //Shapes stored in collections
-    // Shape* shape_collection[]={ &circle1, &oval1 , &circle2, &oval2, &circle3, &oval3};
+    Shape* shape_collection[]={ &circle1, &oval1 , &circle2, &oval2, &circle3, &oval3};
 
-    // for (Shape* ptr: shape_collection)
-    // {
-    //     std::cout << " sizeof(*ptr)) : "<< sizeof(*ptr) << std::endl;
-    //     ptr->draw();
-    // }
+    for (Shape* ptr: shape_collection)
+    {
+        std::cout << " sizeof(*ptr)) : "<< sizeof(*ptr) << std::endl;
+        ptr->draw();
+    }
         
 
     //smart pointers
