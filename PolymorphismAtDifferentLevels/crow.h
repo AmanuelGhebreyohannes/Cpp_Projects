ca988e85baa3f1004e4dcbfbd2d58f28f6188231
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-04-19 15:17:27 -0400

    Polymophism at different levels

---
+++ PolymorphismAtDifferentLevels/crow.h
@@ -0,0 +1,27 @@
+#ifndef CROW_H
+#define CROW_H
+
+#include "bird.h"
+class Crow : public Bird
+{
+    public :
+        Crow() = default;
+        Crow(std::string_view wing_color, std::string_view description)
+            :Bird(wing_color, description)
+            {
+
+            }
+
+        ~Crow() = default;
+
+        virtual void cow() const{
+            std::cout << "Crow::cow called fro crow : " << m_description << std::endl;
+
+        }
+
+        virtual void breath()const override{
+            std::cout << "Crow::breath called for : "<< m_description << std::endl;
+        }
+};
+
+#endif
